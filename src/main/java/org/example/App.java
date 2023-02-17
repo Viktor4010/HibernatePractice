5e3332dddb35ea7c84aa97b60b3cf42096b05708
Viktor4010 <avacode78@gmail.com>
2023-02-17 15:14:27 +0700

    get items for certain Person.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.example.model.Item;
 import org.example.model.Person;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -12,20 +13,25 @@ import java.util.List;
  */
 public class App {
     public static void main(String[] args) {
-        Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+        try (SessionFactory sessionFactory = new Configuration()
+                .addAnnotatedClass(Person.class)
+                .addAnnotatedClass(Item.class)
+                .buildSessionFactory();
+             Session session = sessionFactory.getCurrentSession()) {
 
-        SessionFactory sessionFactory = configuration.buildSessionFactory();
-        Session session = sessionFactory.getCurrentSession();
-
-        try {
             session.beginTransaction();
 
-            session.createQuery("delete Person where age < 30").executeUpdate();
+            // Perform database operations here
+            Person person = session.get(Person.class, 3);
+            System.out.println(person);
+
+            List<Item> items = person.getItems();
+            System.out.println(items);
 
             session.getTransaction().commit();
-
-        } finally {
-            sessionFactory.close();
+        } catch (Exception e) {
+            // Handle exceptions here
         }
     }
 }
+
---
+++ src/main/java/org/example/model/Item.java
@@ -0,0 +1,59 @@
+package org.example.model;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "item")
+public class Item {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "person_id",referencedColumnName = "id")
+    private Person owner;
+
+    @Column(name = "item_name")
+    private String itemName;
+
+    public Item(){}
+
+    public Item(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Person getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Person owner) {
+        this.owner = owner;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id=" + id +
+                ", owner=" + owner +
+                ", itemName='" + itemName + '\'' +
+                '}';
+    }
+}
